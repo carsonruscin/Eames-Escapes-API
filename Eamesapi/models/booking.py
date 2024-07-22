0f14c64cdb56b46ad783dde18226e66b5248db29
Carson Ruscin <carson.xlive@gmail.com>
2024-07-21 19:52:49 -0500

    created models and fixtures, created media root directory for images

---
+++ Eamesapi/models/booking.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+from .property import Property
+
+class Booking(models.Model):
+    property_id = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='bookings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    total_nights = models.IntegerField()
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
