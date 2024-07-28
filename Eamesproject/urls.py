67424d163729709741cf8d7592645d3b944a15e7
Carson Ruscin <carson.xlive@gmail.com>
2024-07-28 15:49:36 -0500

    added register view for handling user login and register requests, added management command to hash passwords

--- Eamesproject/urls.py
+++ Eamesproject/urls.py
@@ -3,13 +3,20 @@ from django.urls import include, path
 from rest_framework import routers
 from django.conf.urls.static import static
 from django.conf import settings
-from Eamesapi.views import serve_landing_page_image
+from Eamesapi.views import *
+from Eamesapi.models import *
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path("register", register_user),
+    path("login", login_user),
+    path("api-token-auth", obtain_auth_token),
+    path("api-auth", include("rest_framework.urls", namespace="rest_framework")),
     path('landing-page-image/', serve_landing_page_image, name='landing_page_image'),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
