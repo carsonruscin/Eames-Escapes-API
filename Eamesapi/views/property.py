15db19bc30a352b5c3b809fac28c5c10b377ac3c
Carson Ruscin <carson.xlive@gmail.com>
2024-08-01 15:30:52 -0500

    removed unused import

--- Eamesapi/views/property.py
+++ Eamesapi/views/property.py
@@ -1,9 +1,7 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from rest_framework import status
-from Eamesapi.models import Property, PropertyType, Amenity, PropertyAmenity
-from rest_framework import permissions
+from Eamesapi.models import Property, PropertyType, Amenity
 
 
 class AmenitySerializer(serializers.ModelSerializer):
